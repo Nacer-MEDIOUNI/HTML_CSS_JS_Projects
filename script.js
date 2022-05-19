3f3a238864a3d068a60ff6334786b092b2cf78eb
Nacer MEDIOUNI <nacer.mediouni@gmail.com>
2022-05-19 22:04:43 +0100

    adding new style and func.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-const loadText = document.querySelector(".loading-text");
-const bg = document.querySelector(".bg");
+const boxes = document.querySelectorAll(".box");
 
-let load = 0;
-let int = setInterval(blurring, 30);
-const scale = (num, in_min, in_max, out_min, out_max) => {
-  return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
-};
+window.addEventListener("scroll", checkBoxes);
 
-function blurring() {
-  load++;
-  if (load > 99) {
-    clearInterval(int);
-  }
-  loadText.innerText = `${load}%`;
-  loadText.style.opacity = scale(load, 0, 100, 1, 0);
-  bg.style.filter = `blur(${scale(load, 0, 100, 100, 0)}px)`;
+checkBoxes();
+
+function checkBoxes() {
+  const triggerBottom = (window.innerHeight / 5) * 4;
+  boxes.forEach((box) => {
+    const boxTop = box.getBoundingClientRect().top;
+
+    if (boxTop < triggerBottom) {
+      box.classList.add("show");
+    } else {
+      box.classList.remove("show");
+    }
+  });
 }
